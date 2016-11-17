47639fbe280d5e4ec06524e563d63ebb81a274ca
Raphaël Dartigues <raphael.dartigues@gmail.com>
2016-11-17 13:33:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,16 +15,16 @@ $("#urlBox").submit(addImage);
 // editing image via css properties
 function editImage() {
 	
-	var gs = $("#gs").val(); // grayscale
-	var blur = $("#blur").val(); // blur
-	var br = $("#br").val(); // brightness
-	var ct = $("#ct").val(); // contrast
-	var huer = $("#huer").val(); //hue-rotate
-	var opacity = $("#opacity").val(); //opacity
-	var invert = $("#invert").val(); //invert
-	var saturate = $("#saturate").val(); //saturate
-	var sepia = $("#sepia").val(); //sepia
-	
+	var gs 		 = $("#gs").val();      // grayscale
+	var blur 	 = $("#blur").val();    // blur
+	var br 		 = $("#br").val();      // brightness
+	var ct 		 = $("#ct").val();      // contrast
+	var huer	 = $("#huer").val();    //hue-rotate
+	var opacity      = $("#opacity").val(); //opacity
+	var invert 	 = $("#invert").val();  //invert
+	var saturate     = $("#saturate").val();//saturate
+	var sepia 	 = $("#sepia").val();   //sepia
+
 	var filter = 	'grayscale(' + gs+
 			'%) blur(' + blur +
 			'px) brightness(' + br +
